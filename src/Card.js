39a6d440e8b4deae05f730d9c9b2bd3a46c40d91
Manish <mapatel_in@hotmail.com>
2023-07-05 00:50:17 +1200

    first commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () =>{
+
+    return(
+        <div className='bg-light-green dib br3'>
+            <img alt='myimage' src='https://www.robohash.org/alien?200x200'></img>
+            <div>
+                <h2>Manish Patel</h2>
+                <p>manish@gmail.com</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Card;    
